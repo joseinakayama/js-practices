63701934a2231b2024c7c679ba70491ffe16cc4f
josei <koga1.jyo.73@gmail.com>
2024-07-16 15:21:11 +0900

    ライブラリのインストール

--- 02.calendar/eslint.config.js
+++ 02.calendar/eslint.config.js
@@ -3,7 +3,12 @@ import pluginJs from "@eslint/js";
 import eslintConfigPrettier from "eslint-config-prettier";
 
 export default [
-  { languageOptions: { globals: globals.nodeBuiltin } },
+  {
+    languageOptions: { globals: globals.nodeBuiltin },
+    env: {
+      node: true, // node の環境変数を追加
+    },
+  },
   pluginJs.configs.recommended,
   eslintConfigPrettier,
 ];
