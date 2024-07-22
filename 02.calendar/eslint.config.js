98e264d0682a87f0d6472ab049da1d74b85aa07c
josei <koga1.jyo.73@gmail.com>
2024-07-22 20:54:42 +0900

    eslintの設定変更

--- 02.calendar/eslint.config.js
+++ 02.calendar/eslint.config.js
@@ -5,9 +5,6 @@ import eslintConfigPrettier from "eslint-config-prettier";
 export default [
   {
     languageOptions: { globals: globals.nodeBuiltin },
-    env: {
-      node: true, // node の環境変数を追加
-    },
   },
   pluginJs.configs.recommended,
   eslintConfigPrettier,
