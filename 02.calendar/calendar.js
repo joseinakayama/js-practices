175098c438094e13bee1d0ac2cc87106928a3a70
josei <koga1.jyo.73@gmail.com>
2024-07-25 10:23:02 +0900

    テンプレートリテラルに変更

--- 02.calendar/calendar.js
+++ 02.calendar/calendar.js
@@ -8,9 +8,9 @@ let date = "";
 if (argv.m == null) {
   date = dayjs();
 } else if (argv.y == null) {
-  date = dayjs("2024-" + argv.m);
+  date = dayjs(`2024- ${argv.m} `);
 } else {
-  date = dayjs(argv.y + "-" + argv.m);
+  date = dayjs(`${argv.y}-${argv.m}`);
 }
 const month = date.format("M");
 const year = date.format("YYYY");
