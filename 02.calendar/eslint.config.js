41a5c8fd7398f97c867fb5562ac59d66d1de63d9
josei <koga1.jyo.73@gmail.com>
2024-07-31 07:14:12 +0900

    初期の設定に戻した。

--- 02.calendar/eslint.config.js
+++ 02.calendar/eslint.config.js
@@ -3,9 +3,7 @@ import pluginJs from "@eslint/js";
 import eslintConfigPrettier from "eslint-config-prettier";
 
 export default [
-  {
-    languageOptions: { globals: globals.nodeBuiltin },
-  },
+  { languageOptions: { globals: globals.nodeBuiltin } },
   pluginJs.configs.recommended,
   eslintConfigPrettier,
 ];
