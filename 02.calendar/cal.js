39439dd88c6e3b0fb57aaf143df315325ef0ead2
josei <koga1.jyo.73@gmail.com>
2024-07-31 15:43:20 +0900

    引数があった場合の処理のロジックの変更

--- 02.calendar/cal.js
+++ 02.calendar/cal.js
@@ -4,18 +4,13 @@ import minimist from "minimist";
 import dayjs from "dayjs";
 
 const argv = minimist(process.argv.slice(2));
-if (argv.m == null && argv.y == null) {
-  var month = dayjs().month() + 1;
-  var year = dayjs().year();
-} else if (argv.y == null) {
-  var month = argv.m;
-  var year = dayjs().year();
-} else if (argv.m == null) {
-  var month = dayjs().month() + 1;
-  var year = argv.y;
-} else {
-  var month = argv.m;
-  var year = argv.y;
+let month = dayjs().month() + 1;
+let year = dayjs().year();
+if (argv.m != null) {
+  month = argv.m;
+}
+if (argv.y != null) {
+  year = argv.y;
 }
 
 console.log(`      ${month}月 ${year}`);
